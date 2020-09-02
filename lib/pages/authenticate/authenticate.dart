20d84cfb4c390bb6339229ba26dcce74b98db323
Richard Huynh <huynhtastic@gmail.com>
2020-09-02 13:47:57 -0500

    Pass flutter analyzer

--- lib/pages/authenticate/authenticate.dart
+++ lib/pages/authenticate/authenticate.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:showsomelove/services/authentication.dart';
 import 'package:showsomelove/utils/required_validator.dart';
